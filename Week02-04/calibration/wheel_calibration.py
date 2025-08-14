546e600ba33586586040d49be885760ca79e3678
KevinHu777 <160016080+KevinHu777@users.noreply.github.com>
2025-08-14 10:54:32 +1000

    Week03_calibration
    WheelCalibration & Camera Calibration were done

--- Week02-04/calibration/wheel_calibration.py
+++ Week02-04/calibration/wheel_calibration.py
@@ -43,7 +43,10 @@ def calibrateWheelRadius():
     num = len(wheel_velocities_range)
     scale = 0
     for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the scale parameter using wheel_vel and delta_time
+        speed = 1 / delta_time
+        scale += speed / wheel_vel
+
+        scale /= num
     print("The scale parameter is estimated as {:.6f} m/ticks.".format(scale))
 
     return scale
@@ -86,7 +89,9 @@ def calibrateBaseline(scale):
     num = len(wheel_velocities_range)
     baseline = 0
     for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the baseline parameter using scale, wheel_vel, and delta_time
+        baseline += (scale * wheel_vel * delta_time)/ np.pi
+        baseline /= num
+        
     print("The baseline parameter is estimated as {:.6f} m.".format(baseline))
 
     return baseline
